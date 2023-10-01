6eb3454ee985694631223b16e9e9a34a6ae4c942
Vishal Kanwar (Student) <vkanwar1@asurite.asu.edu>
2023-10-01 10:45:19 -0700

    Adding Cool Small Stuff to python

---
+++ python/usefullSmallStuff.py
@@ -0,0 +1,17 @@
+# Things that are too small to be their own files, but come in handy
+
+# Integer.max min
+nax = float('-inf')
+
+# Lambda Operations
+operation = lambda x, y: x+y
+operation(1,2)
+
+# Modulo 10^9+7
+M = 1000000007
+res %= M
+
+# Filter Function -> Returns ls
+filter(fucntion, sequence)
+filter(str.isalnum, s)
+filter(lambda x: x%2==0, ls)
